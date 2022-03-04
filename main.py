e79e2809c0e58046273ae844c0274e5311dd033a
UlisesJE <ujuareze1700@alumno.ipn.mx>
2022-03-03 23:08:16 -0600

    sending SMS via the twilio API

--- main.py
+++ main.py
@@ -1,35 +1,51 @@
+from twilio.rest import Client
 import requests
 import os
 from dotenv import load_dotenv
 load_dotenv()
 
 
-OWM_Endpoint=os.getenv("END_POINT")
-api_key=os.getenv("API_KEY")
-parametros={
-    "lat":19.432608,
-    "lon":-99.133209,
-    "appid":api_key,
-    "exclude":"current,minutely,daily"
+OWM_Endpoint = os.getenv("END_POINT")
+api_key = os.getenv("API_KEY")
+account_sid = os.getenv('TWILIO_ACCOUNT_SID')
+auth_token = os.getenv('TWILIO_AUTH_TOKEN')
+my_number=os.getenv("PHONE")
+
+
+parametros = {
+    "lat": 19.432608,
+    "lon": -99.133209,
+    "appid": api_key,
+    "exclude": "current,minutely,daily"
 }
 
-response=requests.get(OWM_Endpoint,params=parametros)
+response = requests.get(OWM_Endpoint, params=parametros)
 response.raise_for_status()
-datos=response.json()
-primeros_12=datos["hourly"][:12]
+datos = response.json()
+primeros_12 = datos["hourly"][:12]
 
-llovera=False
+llovera = False
 for hora in primeros_12:
-    cod_clima=hora["weather"][0]["id"]
+    cod_clima = hora["weather"][0]["id"]
 
-    if int(cod_clima)<700:
-        llovera=True
+    if int(cod_clima) < 700:
+        llovera = True
 
 if llovera:
-    print("Lleva una sombilla")
+    client = Client(account_sid, auth_token)
+    message = client.messages \
+        .create(
+            body="It's going to rain today. Remember to bring an umbrella",
+            from_='+17622165590',
+            to=my_number
+        )
+    print(message.status)
 else:
-    print("No llevará no lleves paraguas")
-
-
-
-# print(datos["hourly"][0]["weather"][0]["id"])
+    client = Client(account_sid, auth_token)
+    message = client.messages \
+        .create(
+            body="It isn't going to rain today. Don't bring an umbrella",
+            from_='+17622165590',
+            to=my_number
+        )
+    print(message.status)
